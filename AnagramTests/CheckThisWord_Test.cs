4b4be1902246c886faf00b333c32ee24c5cca7f3
Jeremy Bowden <jeremybowden@msn.com>
2019-01-15 16:26:40 +0000

    Refactored Unit Test so comments make more sense.

--- AnagramTests/CheckThisWord_Test.cs
+++ AnagramTests/CheckThisWord_Test.cs
@@ -11,17 +11,6 @@ namespace AnagramTests
         public void CheckThisWord_valid_word_is_found()
         {
             // Arrange
-
-            // using Moq is not working because the mock object (mockIDataForCheckWord) is not mutated
-            // when the method-on-test (CheckThisWord) finds a letter in mockIDataForCheckWord.Word that
-            // is in mockIDataForCheckWord.UserText. The algorithm in CheckThisWord() should mutate the
-            // UserText when a letter if found. Works OK in the program, but not in this test.
-            //var mockIDataForCheckWord = new Mock<IDataForCheckWord>();
-            //mockIDataForCheckWord.Setup(m => m.UserText).Returns("to");
-            //mockIDataForCheckWord.Setup(m => m.Word).Returns("toototot");
-            //var testResObj = new CheckWord(mockIDataForCheckWord.Object);
-
-            // So do it the simple way instead
             var testDataForCheckWord = new DataForCheckWord();
             testDataForCheckWord.UserText = "cat";
             testDataForCheckWord.Word = "at";
@@ -54,6 +43,17 @@ namespace AnagramTests
         public void CheckThisWord_invalid_word_containing_valid_letters_is_not_found()
         {
             // Arrange
+
+            // using Moq is not working because the mock object (mockIDataForCheckWord) is not mutated
+            // when the method-on-test (CheckThisWord) finds a letter in mockIDataForCheckWord.Word that
+            // is in mockIDataForCheckWord.UserText. The algorithm in CheckThisWord() should mutate the
+            // UserText when a letter if found. Works OK in the program, but not in this test.
+            //var mockIDataForCheckWord = new Mock<IDataForCheckWord>();
+            //mockIDataForCheckWord.Setup(m => m.UserText).Returns("to");
+            //mockIDataForCheckWord.Setup(m => m.Word).Returns("toototot");
+            //var testResObj = new CheckWord(mockIDataForCheckWord.Object);
+
+            // So do it the simple way instead
             var testDataForCheckWord = new DataForCheckWord();
             testDataForCheckWord.UserText = "to";
             testDataForCheckWord.Word = "tot"; // invalid because each letter in UserText can only be use once
